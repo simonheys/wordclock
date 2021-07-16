f9c1e3e0a36eece53887f7665594a87b0e6a1281
Simon Heys <si@simonheys.com>
2021-07-16 14:04:36 +0100

    Words file parser

---
+++ packages/wordclock-js/src/modules/WordsFileParser.js
@@ -0,0 +1,45 @@
+export const parseJsonUrl = async (url) => {
+  const response = await fetch(url);
+  const json = await response.json();
+  return parseJson(json);
+};
+
+export const parseJson = ({ groups }) => {
+  const label = [];
+  const logic = [];
+  groups.forEach((group) => {
+    const groupLabel = [];
+    const groupLogic = [];
+    group.forEach((entry) => {
+      const type = entry.type;
+      if (type === "item") {
+        const items = entry.items;
+        items.forEach((item) => {
+          const highlight = item.highlight;
+          const text = item.text ?? "";
+          groupLabel.push(text);
+          groupLogic.push(highlight);
+        });
+      } else if (type === "sequence") {
+        const bind = entry.bind;
+        const first = entry.first;
+        const textArray = entry.text;
+        textArray.forEach((text, index) => {
+          const highlight = `${bind}==${first + index}`;
+          groupLabel.push(text);
+          groupLogic.push(highlight);
+        });
+      } else if (type === "space") {
+        const count = entry.count;
+        for (let i = 0; i < count; i++) {
+          groupLabel.push("");
+          groupLogic.push("");
+        }
+      }
+    });
+    logic.push(groupLogic);
+    label.push(groupLabel);
+  });
+
+  return { logic, label };
+};
