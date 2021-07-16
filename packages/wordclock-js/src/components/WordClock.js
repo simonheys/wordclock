1e76fcad822a347b6bb1872e9ad8f013ad9e5520
Simon Heys <si@simonheys.com>
2021-07-16 14:41:42 +0100

    Basic working

--- packages/wordclock-js/src/components/WordClock.js
+++ packages/wordclock-js/src/components/WordClock.js
@@ -1,9 +1,38 @@
 import * as React from "react";
+
 import { useTimeProps } from "../hooks/useTimeProps";
+import * as WordsFileParser from "../modules/WordsFileParser";
+import * as LogicParser from "../modules/LogicParser";
 
 const WordClock = () => {
   const timeProps = useTimeProps();
-  return <pre>{JSON.stringify(timeProps, null, 2)}</pre>;
+  const [logic, setLogic] = React.useState([]);
+  const [label, setLabel] = React.useState([]);
+  const loadJson = async () => {
+    const parsed = await WordsFileParser.parseJsonUrl("/English.json");
+    setLogic(parsed.logic);
+    setLabel(parsed.label);
+  };
+  React.useEffect(() => {
+    loadJson();
+  }, []);
+  return label.map((labelGroup, labelIndex) => {
+    const logicGroup = logic[labelIndex];
+    return labelGroup.map((label, labelGroupIndex) => {
+      if (!label.length) {
+        return null;
+      }
+      const logic = logicGroup[labelGroupIndex];
+      const highlighted = LogicParser.term(logic, timeProps);
+      const style = highlighted ? { color: "red" } : null;
+      return (
+        <span key={`${labelIndex}-${labelGroupIndex}`} style={style}>
+          {label}
+        </span>
+      );
+    });
+  });
+  //   return <pre>{JSON.stringify(timeProps, null, 2)}</pre>;
 };
 
 export default WordClock;
