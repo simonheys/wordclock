0cae0d0948fb976123c5099265d62a0ddcbcfb81
Simon Heys <si@simonheys.com>
2021-07-16 15:19:02 +0100

    WIP

--- packages/wordclock-js/src/components/WordClock.js
+++ packages/wordclock-js/src/components/WordClock.js
@@ -4,18 +4,7 @@ import { useTimeProps } from "../hooks/useTimeProps";
 import * as WordsFileParser from "../modules/WordsFileParser";
 import * as LogicParser from "../modules/LogicParser";
 
-const WordClock = () => {
-  const timeProps = useTimeProps();
-  const [logic, setLogic] = React.useState([]);
-  const [label, setLabel] = React.useState([]);
-  const loadJson = async () => {
-    const parsed = await WordsFileParser.parseJsonUrl("/English.json");
-    setLogic(parsed.logic);
-    setLabel(parsed.label);
-  };
-  React.useEffect(() => {
-    loadJson();
-  }, []);
+const WordClockInner = ({ logic, label, timeProps, fontSize }) => {
   return label.map((labelGroup, labelIndex) => {
     const logicGroup = logic[labelIndex];
     return labelGroup.map((label, labelGroupIndex) => {
@@ -32,6 +21,57 @@ const WordClock = () => {
       );
     });
   });
+};
+
+const WordClock = () => {
+  const timeProps = useTimeProps();
+  const [logic, setLogic] = React.useState([]);
+  const [label, setLabel] = React.useState([]);
+  const wordClockRef = React.useRef(null);
+  const loadJson = async () => {
+    const parsed = await WordsFileParser.parseJsonUrl("/English.json");
+    setLogic(parsed.logic);
+    setLabel(parsed.label);
+  };
+  React.useEffect(() => {
+    loadJson();
+  }, []);
+  const setWordClockRef = React.useCallback(
+    (ref) => {
+      if (ref) {
+        wordClockRef.current = ref;
+        const rect = wordClockRef.current.getBoundingClientRect();
+        console.log(rect);
+      }
+    },
+    [wordClockRef]
+  );
+  return (
+    <div ref={setWordClockRef}>
+      <WordClockInner
+        fontSize={32}
+        logic={logic}
+        label={label}
+        timeProps={timeProps}
+      />
+    </div>
+  );
+  //   return label.map((labelGroup, labelIndex) => {
+  //     const logicGroup = logic[labelIndex];
+  //     return labelGroup.map((label, labelGroupIndex) => {
+  //       if (!label.length) {
+  //         return null;
+  //       }
+  //       const logic = logicGroup[labelGroupIndex];
+  //       const highlighted = LogicParser.term(logic, timeProps);
+  //       const style = highlighted ? { color: "red" } : null;
+  //       return (
+  //         <span key={`${labelIndex}-${labelGroupIndex}`} style={style}>
+  //           {label}
+  //         </span>
+  //       );
+  //     });
+  //   });
   //   return <pre>{JSON.stringify(timeProps, null, 2)}</pre>;
 };
 
